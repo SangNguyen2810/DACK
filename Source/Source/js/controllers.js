ed5a19d4bb05fe213dcd40e31712542caff3ef1b
sonhoang0611 <hoangthaison985@gmail.com>
2016-05-31 23:02:41 +0700

    firebase
    upload data to firebase and hosting to firebase

---
+++ Source/Source/js/controllers.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/* Controllers */
+var app = angular.module("myApp", ["firebase"]);
+// inject $firebaseObject into our controller
+app.controller("mywebCtrl", function($scope, $firebaseObject) {
+			
+	$scope.addSkill = function () {
+
+                if (angular.isDefined($scope.name) && angular.isDefined($scope.score) && $scope.name != '' && $scope.score != '') 
+                {
+                    // ADD A NEW ELEMENT.
+					
+                    $scope.listSkill.push({ name: $scope.name, score: $scope.score });
+
+                    // CLEAR THE FIELDS.
+                    $scope.name = '';
+                    $scope.score = '';
+					
+					window.alert('Bạn đã thêm thành công!');
+                }
+				else{
+					window.alert('Bạn chưa nhập đầy đủ thông tin! Yêu cầu nhập lại!');
+				}
+            };
+			
+	$scope.addProj = function () {
+
+                if (angular.isDefined($scope.proj) && $scope.proj != '') 
+                {
+                    // ADD A NEW ELEMENT.
+					
+                    $scope.listProj.push({ proj: $scope.proj});
+
+                    // CLEAR THE FIELDS.
+                    $scope.proj= '';
+					
+					window.alert('Bạn đã thêm thành công!');
+                }
+				else{
+					window.alert('Bạn chưa nhập đầy đủ thông tin! Yêu cầu nhập lại!');
+				}
+            };
+			
+	$scope.addSum = function () {
+
+                if (angular.isDefined($scope.label) && angular.isDefined($scope.information) && $scope.label != '' && $scope.information != '') 
+                {
+                    // ADD A NEW ELEMENT.
+					
+                    $scope.listSum.push({ label: $scope.label, information: $scope.information });
+
+                    // CLEAR THE FIELDS.
+                    $scope.label = '';
+                    $scope.information = '';
+					
+					window.alert('Bạn đã thêm thành công!');
+                }
+				else{
+					window.alert('Bạn chưa nhập đầy đủ thông tin! Yêu cầu nhập lại!');
+				}
+            };
+			
+	$scope.addExp = function () {
+
+                if (angular.isDefined($scope.title) && angular.isDefined($scope.location1) && angular.isDefined($scope.period) && angular.isDefined($scope.description)
+					&& $scope.title != '' && $scope.location1 != '' && $scope.period != '' && $scope.description != '') 
+                {
+                    // ADD A NEW ELEMENT.
+					
+                    $scope.listExp.push({ title: $scope.title, location1: $scope.location1, period: $scope.period, description: $scope.description });
+
+                    // CLEAR THE FIELDS.
+                    $scope.title = '';
+                    $scope.location1 = '';
+					$scope.period = '';
+					$scope.description = '';
+					
+					window.alert('Bạn đã thêm thành công!');
+                }
+				else{
+					window.alert('Bạn chưa nhập đầy đủ thông tin! Yêu cầu nhập lại!');
+				}
+            };
+			
+	$scope.addSchool = function () {
+
+                if (angular.isDefined($scope.school) && angular.isDefined($scope.field) && angular.isDefined($scope.year)
+					&& $scope.school != '' && $scope.field != '' && $scope.year != '') 
+                {
+                    // ADD A NEW ELEMENT.
+					
+                    $scope.listSchool.push({ school: $scope.school, field: $scope.field, year: $scope.year});
+
+                    // CLEAR THE FIELDS.
+                    $scope.school = '';
+                    $scope.field = '';
+					$scope.year = '';
+					
+					window.alert('Bạn đã thêm thành công!');
+                }
+				else{
+					window.alert('Bạn chưa nhập đầy đủ thông tin! Yêu cầu nhập lại!');
+				}
+            };		
+			
+    var ref = new Firebase("https://sonhoang0611.firebaseio.com");
+	$scope.person = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+	// syncObject.$bindTo($scope, "person");
+	}
+);
+
+
